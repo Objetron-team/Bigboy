6b8424757d581b2af343f9c03ead930b32c0240d
Florian <fl@cortess.eu>
2024-03-23 13:27:35 +0100

    Add missing value and change config

--- code/main/settings/pami/motor_def.h
+++ code/main/settings/pami/motor_def.h
@@ -2,16 +2,16 @@
 // Motor definition
 
 //  Left motor pins
-#define MOTOR_L_PIN_1 25
-#define MOTOR_L_PIN_2 26
+#define MOTOR_L_PIN_1 32
+#define MOTOR_L_PIN_2 33
 
 //  Right motor pins
-#define MOTOR_R_PIN_1 32
-#define MOTOR_R_PIN_2 33
+#define MOTOR_R_PIN_1 25
+#define MOTOR_R_PIN_2 26
 
 //  Global motor parameters
 #define MOTOR_ACCELERATION 20
-#define MOTOR_MAX_SPEED 20
+#define MOTOR_MAX_SPEED 40
 #define MOTOR_MIN_SPEED 0
 #define MOTOR_THRESHOLD_SPEED 3
 
@@ -20,15 +20,17 @@
 
 // Encoder definition
 
-#define ENCODER_L_PIN_A 39
-#define ENCODER_L_PIN_B 36
+#define ENCODER_L_PIN_A 18
+#define ENCODER_L_PIN_B 19
 
-#define ENCODER_R_PIN_A 19
-#define ENCODER_R_PIN_B 18
+#define ENCODER_R_PIN_A 39
+#define ENCODER_R_PIN_B 36
 
 #define ENCODER_RESOLUTION 1786  // pulses per revolution
 #define ENCODER_MAX_FREQ 6400   // Hz
 #define WHEEL_DIAMETER 0.035    // meters    
+#define WHEEL_DISTANCE 0.112 / 2   // meters
+
 
 // PID definition
 
