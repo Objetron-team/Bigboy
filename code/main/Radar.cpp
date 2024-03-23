9b4f78d03464637810329143f703c3dba2777bea
Florian <fl@cortess.eu>
2024-03-23 10:15:26 +0100

    add radar class

---
+++ code/main/Radar.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>;
+
+#define SOUND_SPEED  340.0 / 1000
+#define MEASURE_TIMEOUT 25000UL
+#define MEASURE_INTERVAL 10
+#define OUT_OF_RANGE 1000000
+
+
+class Radar{
+private:
+    /* data */
+
+    int trigger_pin = 0;
+    int echo_pin = 0;
+
+public:
+    Radar(int trigger_pin, int echo_pin){
+        this->trigger_pin = trigger_pin;
+        this->echo_pin = echo_pin;
+    }
+
+    void Init(){
+        pinMode(trigger_pin, OUTPUT);
+        digitalWrite(echo_pin, LOW);
+    }
+
+
+    float GetDistance(){
+
+        digitalWrite(trigger_pin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigger_pin, LOW);
+        
+        long measure = pulseIn(echo_pin, HIGH, MEASURE_TIMEOUT);
+        if(measure == 0){
+            return OUT_OF_RANGE;
+        }
+
+        float distance_mm = measure / 2.0 * SOUND_SPEED;
+        return distance_mm;
+    }
+};
+
