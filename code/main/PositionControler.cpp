647e9d78e760ed5a2b6315d04298520ed4c952be
Florian <fl@cortess.eu>
2024-03-26 09:32:19 +0100

    remove task and value converter definition

--- code/main/PositionControler.cpp
+++ code/main/PositionControler.cpp
@@ -1,167 +1,16 @@
 
 #include "DriveControler.cpp";
+#include "ValueConverter.hpp";
+
+
+#ifndef PositionControler_hpp
+
+#define PositionControler_hpp
 
 // Define the structure of a point.
 struct Point{
     float x;
     float y;
-};
-
-enum TaskType{
-    MOVE,
-    ROTATE,
-};
-
-
-class Task{
-
-    private:
-        TaskType type;
-        Point point;
-
-        float distance_to_travel = 0;
-        float angle_to_rotate = 0;
-
-        Task* next_task = NULL;
-
-        float threshold = 0.05; // in %
-    public:
-
-        int id;
-
-        Task(TaskType type, Point point){
-            this->type = type;
-            this->point = point;
-            this->next_task = NULL;
-        }
-
-        TaskType GetType(){
-            return type;
-        }
-
-        float GetDistance(){
-            return distance_to_travel;
-        }
-
-        float GetAngle(){
-            return angle_to_rotate;
-        }
-
-        void Compute(Point current_point, float current_angle){
-            if(type == MOVE){
-                // Compute the distance to travel
-                distance_to_travel = sqrt(pow(point.x - current_point.x, 2) + pow(point.y - current_point.y, 2));
-                angle_to_rotate = 0;
-            }else if(type == ROTATE){
-
-                // Compute the angle to rotate (angle between the current position and the target position) in degree
-                float dx = point.x - current_point.x;
-                float dy = point.y - current_point.y;
-
-                angle_to_rotate = atan2(dy, dx) * 180 / M_PI;
-
-                // Compute the shortest angle to rotate
-                float angle_diff = angle_to_rotate - current_angle;
-
-                if(angle_diff > 180){
-                    angle_diff -= 360;
-
-                }else if(angle_diff < -180){
-
-                    angle_diff += 360;
-                }
-
-                angle_to_rotate = angle_diff;
-                distance_to_travel = 0;
-
-            }
-        }
-
-        float MoveError(Point current_point){
-            return sqrt(pow(point.x - current_point.x, 2) + pow(point.y - current_point.y, 2));
-        }
-
-        float RotateError(Point current_point){
-            return abs(atan2(point.y - current_point.y, point.x - current_point.x) * 180 / M_PI);
-        }
-
-        bool IsDone(Point current_point){
-
-            if(type == MOVE){
-                return sqrt(pow(point.x - current_point.x, 2) + pow(point.y - current_point.y, 2)) < 5;
-            }else if(type == ROTATE){
-                return abs(atan2(point.y - current_point.y, point.x - current_point.x) * 180 / M_PI) < 5;
-            }
-
-            return false;
-        }
-
-        void SetNextTask(Task* next_task){
-            
-            // add the next task to the end of the list
-            Task* last_task = GetLastTask();
-
-            next_task->id = last_task->id + 1;
-
-            last_task->next_task = next_task;
-        }
-
-        bool HasNextTask(){
-            return next_task != NULL;
-        }
-
-        Task* GetNextTask(){
-            return next_task;
-        }
-
-        Task* GetLastTask(){
-            Task* task = this;
-            while(task->HasNextTask()){
-                task = task->GetNextTask();
-            }
-            return task;
-        }
-};
-
-class ValueConverter{
-
-    private:
-        int pulse_per_turn = 1000;
-        float wheel_diameter = 1; // in meter
-        float wheel_distance = 1; // in meter
-
-    public:
-
-    ValueConverter(int pulse_per_turn, float wheel_diameter, float wheel_distance) {
-        this->pulse_per_turn = pulse_per_turn;
-        this->wheel_diameter = wheel_diameter;
-        this->wheel_distance = wheel_distance;
-    }
-
-    float PulseToDistanceCM(int pulse){
-        return pulse * 100 * wheel_diameter * M_PI / pulse_per_turn;
-    }
-
-    float PulseToAngle(int pulse){
-
-        float arc_length = PulseToDistanceCM(pulse) / 100; // in meter
-        float angle_rad = arc_length / wheel_distance; // in radian
-
-        return angle_rad * 180 / M_PI;
-    }
-
-    float AngleToPulse(float angle){
-        // in degree to radian
-        float angle_rad = angle * M_PI / 180;
-        float arc_length = wheel_distance * angle_rad; // in meter (wheel_distance is in meter)
-
-        return DistanceCMToPulse(arc_length * 100);
-    }
-
-    float DistanceCMToPulse(float distance){
-        return distance * pulse_per_turn / (wheel_diameter * 100 * M_PI);
-    }
-
 };
 
 class PositionControler{
@@ -230,8 +79,6 @@ class PositionControler{
 
             }
 
-            float dt = (micros() - last_update_time) / 1000000.0;
-
             // integrate the position
             float distance = valueConverter->PulseToDistanceCM(driveControler->GetDistance());  // in centimeter
             float angle = valueConverter->PulseToAngle(driveControler->GetAngle());             // in degree
@@ -247,8 +94,6 @@ class PositionControler{
 
             last_distance = distance;
             last_angle = angle;
-
-            last_update_time = micros();
 
             driveControler->Update();
         }
@@ -342,7 +187,4 @@ class PositionControler{
 };
 
 
-
-
-
-
+#endif
