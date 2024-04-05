8d087d5ae865628a04e0ddeff272a2ffe1d6d449
Florian <fl@cortess.eu>
2024-04-05 17:13:18 +0200

    update PID value for drive controller

--- code/main/settings/pami/drive_def.h
+++ code/main/settings/pami/drive_def.h
@@ -1,9 +1,9 @@
 // define PID value for the Drive controller
 
-#define DISTANCE_KP 0.05f
+#define DISTANCE_KP 0.06f
 #define DISTANCE_KI 0.0f
 #define DISTANCE_KD 0.0f
 
-#define ANGLE_KP 0.08f
+#define ANGLE_KP 0.5f
 #define ANGLE_KI 0.0f
 #define ANGLE_KD 0.0f
