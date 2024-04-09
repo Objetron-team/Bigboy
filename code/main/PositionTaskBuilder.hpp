4e53ebb98733b9369faee13c190f6bc9c16229b2
Florian <fl@cortess.eu>
2024-04-09 11:38:04 +0200

    Reformat

--- code/main/PositionTaskBuilder.hpp
+++ code/main/PositionTaskBuilder.hpp
@@ -10,41 +10,45 @@
 #include "Tasks/RotateTask.hpp"
 #include "Tasks/WaitTask.hpp"
 
-class PositionTaskBuilder{
+class PositionTaskBuilder
+{
 private:
+    PositionControler *positionControler;
+    DriveControler *driveControler;
+    ValueConverter *valueConverter;
 
-    PositionControler* positionControler;
-    DriveControler* driveControler;
-    ValueConverter* valueConverter;
-    
 public:
+    PositionTaskBuilder(PositionControler *positionControler, DriveControler *driveControler, ValueConverter *valueConverter)
+    {
 
-    PositionTaskBuilder(PositionControler* positionControler, DriveControler* driveControler, ValueConverter* valueConverter){
-        
         this->positionControler = positionControler;
         this->driveControler = driveControler;
         this->valueConverter = valueConverter;
     }
 
-    MoveTask* CreateMoveTask(Point target){
+    MoveTask *CreateMoveTask(Point target)
+    {
         return new MoveTask(positionControler, driveControler, valueConverter, target);
     }
 
-    RotateTask* CreateRotateTask(Point target){
+    RotateTask *CreateRotateTask(Point target)
+    {
         return new RotateTask(positionControler, driveControler, valueConverter, target);
     }
 
-    BasicTask* CreateTasksFromPoints(Point* points, int size){
-        BasicTask* first_task = CreateMoveTask(points[0]);
+    BasicTask *CreateTasksFromPoints(Point *points, int size)
+    {
+        BasicTask *first_task = CreateMoveTask(points[0]);
 
-        for(int i = 1; i < size; i++){
+        for (int i = 1; i < size; i++)
+        {
             Point target = points[i];
 
-            WaitTask* wait_task_1 = new WaitTask(positionControler, driveControler, valueConverter);
-            WaitTask* wait_task_2 = new WaitTask(positionControler, driveControler, valueConverter);
+            WaitTask *wait_task_1 = new WaitTask(driveControler);
+            WaitTask *wait_task_2 = new WaitTask(driveControler);
 
-            RotateTask* rotate_task = CreateRotateTask(target);
-            MoveTask* move_task = CreateMoveTask(target);
+            RotateTask *rotate_task = CreateRotateTask(target);
+            MoveTask *move_task = CreateMoveTask(target);
 
             first_task->AddTask(rotate_task);
             first_task->AddTask(wait_task_1);
@@ -53,5 +57,5 @@ public:
         }
 
         return first_task;
-    }   
+    }
 };
