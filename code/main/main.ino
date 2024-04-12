553fe9afb4aca44df47c4f95fb17788f5819657f
Florian <fl@cortess.eu>
2024-04-12 15:43:59 +0200

    use radar map

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,20 +1,20 @@
 #define SAMPLE_TIME 15
 
-#define IS_MAIN true
-#define PAMI_TYPE 0 // 0 -> noir 1 -> gris
+#define IS_MAIN false
+#define PAMI_TYPE 1 // 0 -> noir 1 -> gris
 
 #if IS_MAIN
     #include "settings/main/motor_def.h";
-    #include "settings/main/drive_def.h";
-    #include "settings/main/bluetooth_config.h";
-    #include "settings/main/radar.h";
-    #include "settings/main/claw.h";
-    #include "settings/main/arm.h";
+#include "settings/main/drive_def.h";
+#include "settings/main/bluetooth_config.h";
+#include "settings/main/radar.h";
+#include "settings/main/claw.h";
+#include "settings/main/arm.h";
 #else
     #include "settings/pami/motor_def.h";
-    #include "settings/pami/drive_def.h";
-    #include "settings/pami/bluetooth_config.h";
-    #include "settings/pami/radar.h";
+#include "settings/pami/drive_def.h";
+#include "settings/pami/bluetooth_config.h";
+#include "settings/pami/radar.h";
 #endif
 
 #include "PIDMotor.hpp"
@@ -30,7 +30,7 @@
 
 #if IS_MAIN
     Arm myArm(PIN_ARM, ARM_TIME);
-    Claw myClaw(PIN_CLAW_1, PIN_CLAW_2, CLAW_TIME);
+Claw myClaw(PIN_CLAW_1, PIN_CLAW_2, CLAW_TIME);
 #else
     Radar radar(TRIGGER_PIN, ECHO_PIN);
 #endif
@@ -50,39 +50,42 @@ PIDMotor motorR(MOTOR_R_PIN_1, MOTOR_R_PIN_2, MOTOR_ACCELERATION, MOTOR_MAX_SPEE
 
 ValueConverter valueConverter(ENCODER_RESOLUTION, WHEEL_DIAMETER, WHEEL_DISTANCE);
 
-DriveControler driveControler(&motorL, &motorR);
-PositionControler positionControler(&driveControler, ENCODER_RESOLUTION, WHEEL_DIAMETER, WHEEL_DISTANCE);
+DriveControler driveControler(& motorL, & motorR);
+PositionControler positionControler(& driveControler, ENCODER_RESOLUTION, WHEEL_DIAMETER, WHEEL_DISTANCE);
 
-TaskControler taskControler(&positionControler, &driveControler, &valueConverter);
+TaskControler taskControler(& positionControler, & driveControler, & valueConverter);
 
-PositionTaskBuilder positionTaskBuilder(&positionControler, &driveControler, &valueConverter);
+PositionTaskBuilder positionTaskBuilder(& positionControler, & driveControler, & valueConverter);
 
 void setup()
 {
-
-#if IS_MAIN
-    myClaw.Init();
+    
+    #if IS_MAIN
+        myClaw.Init();
     myArm.Init();
     // Radar setup
     Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
-#else
-    radar.Init();
-#endif
-
+    #else
+        radar.Init();
+    driveControler.AddRadar( & radar, FRONT);
+    #endif
+    
     // Motor setup
     motorL.InitEncoder(ENCODER_L_PIN_A, ENCODER_L_PIN_B, ENCODER_MAX_FREQ_L, ENCODER_RESOLUTION, WHEEL_DIAMETER);
     motorR.InitEncoder(ENCODER_R_PIN_A, ENCODER_R_PIN_B, ENCODER_MAX_FREQ_R, ENCODER_RESOLUTION, WHEEL_DIAMETER);
-
+    
     // PID setup
     motorL.InitPID(LS_PID_KP, LS_PID_KI, LS_PID_KD, SAMPLE_TIME);
     motorR.InitPID(RS_PID_KP, RS_PID_KI, RS_PID_KD, SAMPLE_TIME);
-
+    
     // DriveControler setup
     driveControler.InitPid(DISTANCE_KP, DISTANCE_KI, DISTANCE_KD, ANGLE_KP, ANGLE_KI, ANGLE_KD, SAMPLE_TIME);
-
+    
+    
+    
     Serial.begin(115200);
     SerialBT.begin(device_name); // Bluetooth device name
-
+    
     taskControler.SetAutoMode(false);
 }
 
@@ -91,11 +94,11 @@ double global_target_2 = 0;
 
 void SerialCommande()
 {
-
+    
     if (SerialBT.available() > 0)
     {
         // if(Serial.available() > 0) {
-
+        
         String commande = SerialBT.readStringUntil('\n');
         // String commande = Serial.readStringUntil('\n');
         char commande_type = commande.charAt(0);
@@ -106,7 +109,7 @@ void SerialCommande()
                     // create Points array  blue 1
                     Point points[2] = {
                         {0,0} ,
-                        {20, -90} ,
+                        {20, -100} ,
                     };
                     
                     BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,2);
@@ -119,7 +122,7 @@ void SerialCommande()
                     // create Points array bleu 2
                     Point points[2] = {
                         {0,0} ,
-                        {75, 140} ,
+                        {75, 160} ,
                     };
                     
                     BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,2);
@@ -130,13 +133,12 @@ void SerialCommande()
                 case'q':
                 {
                     // create Points array yellow 1
-                    Point points[3] = {
+                    Point points[2] = {
                         {0,0} ,
-                        {20,0} ,
-                        {20, 90} ,
+                        {20, 100} ,
                     };
                     
-                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,3);
+                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,2);
                     
                     taskControler.AddTask(task);
                     break;
@@ -144,13 +146,12 @@ void SerialCommande()
                 case'd':
                 {
                     // create Points array yellow 2
-                    Point points[3] = {
+                    Point points[2] = {
                         {0,0} ,
-                        {75,0} ,
-                        {75, -140} ,
+                        {75, -160} ,
                     };
                     
-                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,3);
+                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,2);
                     
                     taskControler.AddTask(task);
                     break;
@@ -169,9 +170,8 @@ void SerialCommande()
             case 'b':
                 {
                     // create Points array
-                    Point points[7] = {
+                    Point points[6] = {
                         {0,0} ,
-                        //{40,0},
                         {80,0} ,
                         {80,40} ,
                         {40,40} ,
@@ -179,7 +179,7 @@ void SerialCommande()
                         {0,0}
                     };
                     
-                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,7);
+                    BasicTask * task = positionTaskBuilder.CreateTasksFromPoints(points,6);
                     
                     taskControler.AddTask(task);
                     break;
@@ -204,8 +204,8 @@ void SerialCommande()
                 break;
             
             default:
-
-              break;
+            
+            break;
         }
     }
 }
@@ -220,13 +220,13 @@ int processBuffer()
         if (buffer[i] == '\n')
         {
             // If newline character is encountered, print the message and reset buffer
-
+            
             parsed_number = atoi(buffer); // Convert the buffer content to an integer
             if (parsed_number != 0)
             {
                 number = parsed_number; // Update the global variable only if a valid number is parsed
             }
-
+            
             if (number < 30)
             {
                 driveControler.UrgentStop();
@@ -239,7 +239,7 @@ int processBuffer()
             Serial.println();
             Serial.write(buffer, i + 1); // Print the message until the newline character
             Serial.println();
-
+            
             // Move remaining data to the beginning of the buffer
             memmove(buffer, buffer + i + 1, bufferIndex - i - 1);
             bufferIndex -= i + 1;
@@ -252,45 +252,37 @@ int processBuffer()
 void loop()
 {
     SerialCommande();
-
-#if IS_MAIN
-
-    while (Serial2.available() > 0)
+    
+    #if IS_MAIN
+        
+        while(Serial2.available() > 0)
     {
         char incomingByte = Serial2.read();
-
+        
         // Store incoming byte in the buffer
         buffer[bufferIndex++] = incomingByte;
-
+        
         // Check if the buffer is full
         if (bufferIndex >= BUFFER_SIZE)
         {
             processBuffer(); // Process the buffer when it's full
         }
     }
-
+    
     // Process remaining data in the buffer
     if (bufferIndex > 0)
     {
         processBuffer();
     }
-
-    #else
-
-        double distance_mm = radar.GetDistance();
-
-        if (distance_mm < 200)
-        {
-            driveControler.UrgentStop();
-        }
+    
     #endif
-
+    
     taskControler.Update();
     // taskControler.Debug();
-
+    
     // Serial.println(MAX_SPEED_PULSE);
-
-    // driveControler.Debug();
-
+    
+    //driveControler.Debug();
+    
     delay(5);
 }
