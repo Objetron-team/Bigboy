78d83616e9670182854c7be897c5f58750da4e7e
Florian <fl@cortess.eu>
2024-04-19 15:39:22 +0200

    Implement Slave

--- code/main/ESPNowSlave.hpp
+++ code/main/ESPNowSlave.hpp
@@ -5,25 +5,103 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+// #include "TaskControler.hpp"
+// #include "PositionTaskBuilder.hpp"
+
 class ESPNowSlave
 {
 private:
 public:
-    ESPNowSlave(/* args */) {}
+    static TaskControler *taskControler_esp;
+    static PositionTaskBuilder *positionTaskBuilder_esp;
+
+    ESPNowSlave(TaskControler *taskControler_, PositionTaskBuilder *positionTaskBuilder_)
+    {
+        taskControler_esp = taskControler_;
+        positionTaskBuilder_esp = positionTaskBuilder_;
+    }
+
+    static void ProcessCMD(command cmd)
+    {
+
+        Serial.print("Processing command: ");
+        Serial.println(cmd);
+
+        switch (cmd)
+        {
+        case START:
+            taskControler_esp->Start();
+            break;
+        case AUTO_ON:
+            taskControler_esp->SetAutoMode(true);
+            break;
+        case AUTO_OFF:
+            taskControler_esp->SetAutoMode(false);
+            break;
+        case YELLOW_ONE:
+        {
+            Point points[2] = {
+                {0, 0},
+                {10, 100},
+            };
+
+            BasicTask *task = positionTaskBuilder_esp->CreateTasksFromPoints(points, 2);
+
+            taskControler_esp->AddTask(task);
+            break;
+        }
+        case YELLOW_TWO:
+        {
+            Point points[3] = {
+                {0, 0},
+                {10, 0},
+                {95, -160},
+            };
+
+            BasicTask *task = positionTaskBuilder_esp->CreateTasksFromPoints(points, 3);
+
+            taskControler_esp->AddTask(task);
+            break;
+        }
+        case BLUE_ONE:
+        {
+            Point points[2] = {
+                {0, 0},
+                {10, -100},
+            };
+
+            BasicTask *task = positionTaskBuilder_esp->CreateTasksFromPoints(points, 2);
+
+            taskControler_esp->AddTask(task);
+            break;
+        }
+        case BLUE_TWO:
+        {
+            Point points[3] = {
+                {0, 0},
+                {10, 0},
+                {95, 160},
+            };
+
+            BasicTask *task = positionTaskBuilder_esp->CreateTasksFromPoints(points, 3);
+
+            taskControler_esp->AddTask(task);
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
 
     static void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
     {
         command_data myData;
-
         memcpy(&myData, incomingData, sizeof(myData));
-        Serial.print("Bytes received: ");
-        Serial.println(len);
-        Serial.print("cmd_1 : ");
-        Serial.println(myData.cmd_1);
-        Serial.print("cmd_2 : ");
-        Serial.println(myData.cmd_2);
-        Serial.print("cmd_3 : ");
-        Serial.println(myData.cmd_3);
+
+        ProcessCMD(myData.cmd_1);
+        ProcessCMD(myData.cmd_2);
+        ProcessCMD(myData.cmd_3);
     }
 
     void Init()
@@ -38,6 +116,9 @@ public:
 
         esp_now_register_recv_cb(OnDataRecv);
 
-        Serial.println("ESPNowMaster initialized");
+        Serial.println("ESPNowSlave initialized");
     }
 };
+
+TaskControler *ESPNowSlave::taskControler_esp = nullptr; // Initialize to nullptr
+PositionTaskBuilder *ESPNowSlave::positionTaskBuilder_esp = nullptr;
